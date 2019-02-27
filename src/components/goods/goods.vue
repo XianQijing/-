3c762424d1a0e08f7e224909485cc4a749cc4d73
XianQijing <154122662@qq.com>
2019-02-27 18:12:22 +0800

    better-scroll

--- src/components/goods/goods.vue
+++ src/components/goods/goods.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="goods">
-    <div class="menu-wrapper">
+    <div class="menu-wrapper" ref="menuWrapper">
       <ul>
         <li v-for="item in goods" :key="item.name" class="meun-item">
           <span class="text border-bottom">
@@ -10,18 +10,24 @@
         </li>
       </ul>
     </div>
-    <div class="foods-wrapper">
+    <div class="foods-wrapper" ref="foodWrapper">
       <ul>
         <li v-for="item in goods" :key="item.foods.name">
           <h1 class="title">{{item.name}}</h1>
           <ul>
-            <li v-for="food in item.foods" :key="food.name" class="food-item">
+            <li v-for="food in item.foods" :key="food.name" class="food-item border-1px">
               <div class="icon">
-                <img :src="food.icon" alt="">
+                <img :src="food.icon" width="57" height="57px">
               </div>
               <div class="content">
                 <h2 class="name">{{food.name}}</h2>
                 <p class="desc">{{food.description}}</p>
+                <div class="extra">
+                  <span class="count">月售{{food.sellCount}}</span><span>好评率{{food.rating}}%</span>
+                </div>
+                <div class="price">
+                  <span class="now">￥{{food.price}}</span><span class="old" v-show="food.oldPrice">{{food.oldPrice}}</span>
+                </div>
               </div>
             </li>
           </ul>
@@ -32,6 +38,8 @@
 </template>
 
 <script>
+import BScroll from 'better-scroll'
+
 export default {
   name: 'goods',
   data () {
@@ -43,7 +51,16 @@ export default {
     this.classMap = ['decrease', 'special', 'discount', 'invoice', 'guarantee']
     this.$axios.get('/api/goods').then(res => {
       this.goods = res.data.data
+      this.$nextTick(() => {
+        this._initScroll()
+      })
     })
+  },
+  methods: {
+    _initScroll () {
+      this.menuScroll = new BScroll(this.$refs.menuWrapper, {})
+      this.foodsScroll = new BScroll(this.$refs.foodWrapper)
+    }
   }
 }
 </script>
@@ -92,4 +109,48 @@ export default {
         vertical-align middle
   .foods-wrapper
     flex 1
+    .title
+      padding-left 14px
+      height 26px
+      line-height 26px
+      border-left 2px solid #d9dde1
+      font-size 12px
+      color rgb(147, 153, 159)
+      background #f3f5f7
+    .food-item
+      display flex
+      margin 18px
+      padding-bottom 18px
+      border-1px(rgba(7, 17, 27, 0.1))
+      &:last-child
+        border-none()
+        margin-bottom 0
+      .icon
+        flex 0 0 57px
+        margin-right 10px
+      .content
+        flex 1
+        .name
+          margin 2px 0 8px 0
+          line-height 14px
+          height 14px
+          font-size 14px
+          color rgb(7, 17, 27)
+        .desc, .extra
+          line-height 10px
+          font-size 10px
+          color rgb(147, 159, 159)
+        .desc
+          margin-bottom 8px
+          line-height 12px
+        .extra
+          .count
+            margin-right 12px
+        .price
+          font-weight 700
+          line-height 24px
+          .now
+            margin-right 8px
+            font-size 14px
+            color rgb(240, 20, 20)
 </style>
